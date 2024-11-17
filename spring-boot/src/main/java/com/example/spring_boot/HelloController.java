c80ae084a97856738e00b68949ba5d299c2736e3
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2024-11-17 13:14:41 -0600

    extension of application.java

--- spring-boot/src/main/java/com/example/spring_boot/HelloController.java
+++ spring-boot/src/main/java/com/example/spring_boot/HelloController.java
@@ -1,11 +1,12 @@
-package com.example.springboot;
+package com.example.spring_boot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// @RestController annotation tells Spring that this code describes an endpoint that should be made available over the web.
 @RestController
 public class HelloController {
-
+    //@GetMapping maps / to the index() method
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
